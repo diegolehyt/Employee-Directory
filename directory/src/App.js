80cb62d7740a4ec515e13b26d6f7be2641fbfb0d
Diego Lehyt <diegolehy00@gmail.com>
2020-06-22 18:28:22 -0400

    all categories sort ascending order added

--- directory/src/App.js
+++ directory/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     results: []
   };
 
+  // Sort BY functions
   sortByName = () => {
     function compare (a, b) {
       const bandA = a.fullName.toUpperCase();
@@ -34,7 +35,90 @@ class App extends Component {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const players = this.state.players.sort(compare)
     console.log(players.sort(compare));
-    // const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  sortByShirt = () => {
+    function compare (a, b) {
+      const bandA = a.shirtNumber;
+      const bandB = b.shirtNumber;
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  sortByAge = () => {
+    function compare (a, b) {
+      const bandA = a.age;
+      const bandB = b.age;
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  sortByCountry = () => {
+    function compare (a, b) {
+      const bandA = a.nationality.toUpperCase();
+      const bandB = b.nationality.toUpperCase();
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  sortByPosition = () => {
+    function compare (a, b) {
+      const bandA = a.position.toUpperCase();
+      const bandB = b.position.toUpperCase();
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
     // Set this.state.friends equal to the new friends array
     this.setState({ players });
   };
@@ -68,7 +152,13 @@ class App extends Component {
       {
         this.state.results.length > 0
         ? <List>
-            <ListHead sortByName={this.sortByName}></ListHead>
+            <ListHead 
+              sortByName={this.sortByName}
+              sortByShirt={this.sortByShirt}
+              sortByAge={this.sortByAge}
+              sortByCountry={this.sortByCountry}
+              sortByPosition={this.sortByPosition}
+            />
             {this.state.results.map(friend => (
               <FriendCard
                 name={friend.fullName}
@@ -82,7 +172,13 @@ class App extends Component {
 
           </List> 
         : <List>
-            <ListHead sortByName={this.sortByName}></ListHead>
+            <ListHead 
+              sortByName={this.sortByName}
+              sortByShirt={this.sortByShirt}
+              sortByAge={this.sortByAge}
+              sortByCountry={this.sortByCountry}
+              sortByPosition={this.sortByPosition}
+            />
             {this.state.players.map(friend => (
               <FriendCard
                 name={friend.fullName}
