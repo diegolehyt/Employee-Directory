990bb8c5f0275aedbd24c9cee07e0f3f41ba6673
Diego Lehyt <diegolehy00@gmail.com>
2020-06-22 18:52:55 -0400

    all categories sort descending added

--- directory/src/App.js
+++ directory/src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
   };
 
   // Sort BY functions
+  // Name Ascending
   sortByName = () => {
     function compare (a, b) {
       const bandA = a.fullName.toUpperCase();
@@ -38,7 +39,29 @@ class App extends Component {
     // Set this.state.friends equal to the new friends array
     this.setState({ players });
   };
+  // Name Descending
+  sortByNameD = () => {
+    function compare (a, b) {
+      const bandA = a.fullName.toUpperCase();
+      const bandB = b.fullName.toUpperCase();
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison * -1;
+    }
 
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  // Shirt Number Ascending
   sortByShirt = () => {
     function compare (a, b) {
       const bandA = a.shirtNumber;
@@ -60,6 +83,29 @@ class App extends Component {
     this.setState({ players });
   };
 
+  // Shirt Number Descending
+  sortByShirtD = () => {
+    function compare (a, b) {
+      const bandA = a.shirtNumber;
+      const bandB = b.shirtNumber;
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison * -1;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  // Age Ascending
   sortByAge = () => {
     function compare (a, b) {
       const bandA = a.age;
@@ -81,6 +127,29 @@ class App extends Component {
     this.setState({ players });
   };
 
+  // Age Descending
+  sortByAgeD = () => {
+    function compare (a, b) {
+      const bandA = a.age;
+      const bandB = b.age;
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison * -1;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  // Country Ascending
   sortByCountry = () => {
     function compare (a, b) {
       const bandA = a.nationality.toUpperCase();
@@ -102,6 +171,29 @@ class App extends Component {
     this.setState({ players });
   };
 
+  // Country Descending
+  sortByCountryD = () => {
+    function compare (a, b) {
+      const bandA = a.nationality.toUpperCase();
+      const bandB = b.nationality.toUpperCase();
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison * -1;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  // Position Ascending
   sortByPosition = () => {
     function compare (a, b) {
       const bandA = a.position.toUpperCase();
@@ -114,6 +206,28 @@ class App extends Component {
         comparison = -1;
       }
       return comparison;
+    }
+
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const players = this.state.players.sort(compare)
+    console.log(players.sort(compare));
+    // Set this.state.friends equal to the new friends array
+    this.setState({ players });
+  };
+
+  // Position Descending
+  sortByPositionD = () => {
+    function compare (a, b) {
+      const bandA = a.position.toUpperCase();
+      const bandB = b.position.toUpperCase();
+    
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison * -1;
     }
 
     // Filter this.state.friends for friends with an id not equal to the id being removed
@@ -154,10 +268,15 @@ class App extends Component {
         ? <List>
             <ListHead 
               sortByName={this.sortByName}
+              sortByNameD={this.sortByNameD}
               sortByShirt={this.sortByShirt}
+              sortByShirtD={this.sortByShirtD}
               sortByAge={this.sortByAge}
+              sortByAgeD={this.sortByAgeD}
               sortByCountry={this.sortByCountry}
+              sortByCountryD={this.sortByCountryD}
               sortByPosition={this.sortByPosition}
+              sortByPositionD={this.sortByPositionD}
             />
             {this.state.results.map(friend => (
               <FriendCard
@@ -174,10 +293,15 @@ class App extends Component {
         : <List>
             <ListHead 
               sortByName={this.sortByName}
+              sortByNameD={this.sortByNameD}
               sortByShirt={this.sortByShirt}
+              sortByShirtD={this.sortByShirtD}
               sortByAge={this.sortByAge}
+              sortByAgeD={this.sortByAgeD}
               sortByCountry={this.sortByCountry}
+              sortByCountryD={this.sortByCountryD}
               sortByPosition={this.sortByPosition}
+              sortByPositionD={this.sortByPositionD}
             />
             {this.state.players.map(friend => (
               <FriendCard
--- directory/src/components/ListHead/index.js
+++ directory/src/components/ListHead/index.js
@@ -5,6 +5,10 @@ const styles = {
   iconS: {
     fontSize: '24px',
     color: 'lightgreen'
+  },
+  iconD: {
+    fontSize: '24px',
+    color: 'coral'
   }
 }
 
@@ -12,11 +16,11 @@ function ListHead(props) {
   return (
     <tr>
       <th>Img</th>
-      <th>Name <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByName()} href=''></i></th>
-      <th>Position <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByPosition()} href=''></i></th>
-      <th>Shirt # <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByShirt()} href=''></i></th>
-      <th>Country <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByCountry()} href=''></i></th>
-      <th>Age <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByAge()} href=''></i></th>
+      <th>Name <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByName()} href=''></i> <i className='fas fa-chevron-circle-down' style={styles.iconD} onClick={() => props.sortByNameD()} href=''></i></th>
+      <th>Position <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByPosition()} href=''></i> <i className='fas fa-chevron-circle-down' style={styles.iconD} onClick={() => props.sortByPositionD()} href=''></i></th>
+      <th>Shirt # <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByShirt()} href=''></i> <i className='fas fa-chevron-circle-down' style={styles.iconD} onClick={() => props.sortByShirtD()} href=''></i></th>
+      <th>Country <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByCountry()} href=''></i> <i className='fas fa-chevron-circle-down' style={styles.iconD} onClick={() => props.sortByCountryD()} href=''></i></th>
+      <th>Age <i className='fas fa-chevron-circle-up' style={styles.iconS} onClick={() => props.sortByAge()} href=''></i> <i className='fas fa-chevron-circle-down' style={styles.iconD} onClick={() => props.sortByAgeD()} href=''></i></th>
     </tr>
   );
 }
